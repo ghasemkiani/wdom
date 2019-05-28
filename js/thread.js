4def41f22d0f97f20f984ea4e2df1481be0c4382
Ghasem Kiani <ghasemkiani@gmail.com>
2019-05-28 17:17:01 +0430

    add resolve/reject to js/thread

--- js/thread.js
+++ js/thread.js
@@ -5,21 +5,51 @@ const {Base} = require("@ghasemkiani/commonbase/base");
 const {Script} = require("@ghasemkiani/wdom/js/script");
 
 class Thread extends Base {
+	get promises() {
+		if(!this._promises) {
+			this._promises = {};
+		}
+		return this._promises;
+	}
+	set promises(promises) {
+		this._promises = promises;
+	}
 	get worker() {
 		if(!this._worker) {
 			this._worker = this.createWorker(new Script().add(arg => {
 				let call = (method, ...rest) => {
 					postMessage(JSON.stringify([method, ...rest]));
 				};
-				onmessage = ({data}) => {
-					[f, ...rest] = JSON.parse(data);
+				onmessage = async ({data}) => {
+					let [key, f, ...rest] = JSON.parse(data);
 					f = eval(f);
-					f(call, ...rest);
+					try {
+						let result = await f(call, ...rest);
+						call("resolve", key, result);
+					} catch(e) {
+						call("reject", key, e.message);
+					}
 				};
 			}, {}).dataUri);
 			this._worker.onmessage = ({data}) => {
 				let [method, ...rest] = JSON.parse(data);
-				this[method](...rest);
+				if(method === "resolve") {
+					let [key, result] = rest;
+					let promise = this.promises[key];
+					if(promise) {
+						promise.resolve(result);
+						delete this.promises[key];
+					}
+				} else if(method === "reject") {
+					let [key, message] = rest;
+					let promise = this.promises[key];
+					if(promise) {
+						promise.reject(message);
+						delete this.promises[key];
+					}
+				} else {
+					this[method](...rest);
+				}
 			};
 		}
 		return this._worker;
@@ -31,7 +61,10 @@ class Thread extends Base {
 		return new Worker(dataUri);
 	}
 	work(f, ...rest) {
-		this.worker.postMessage(JSON.stringify([`(${f.toString()})`, ...rest]));
+		f = f || (() => {});
+		let key = cutil.srand(16);
+		this.promises[key] = new Promise();
+		this.worker.postMessage(JSON.stringify([key, `(${f.toString()})`, ...rest]));
 	}
 	terminate() {
 		this.worker.terminate();
@@ -39,6 +72,7 @@ class Thread extends Base {
 }
 cutil.extend(Thread.prototype, {
 	_worker: null,
+	_promises: null,
 });
 
 module.exports = {Thread};
